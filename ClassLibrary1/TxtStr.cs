33fd73484dc2cd4285b45f19bfff3fb61d80c794
ReyMax <ReyMax@DESKTOP-AUHV6S9>
2018-01-27 18:23:10 +0800

    1

--- ClassLibrary1/TxtStr.cs
+++ ClassLibrary1/TxtStr.cs
@@ -20,13 +20,13 @@ namespace DLL
             return strLine;
         }
 
-        public static string[] ReadLine(string FilePath)
+        public static string[] ReadLine(string FilePath, Encoding encoding)
         {
             FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            StreamReader sr = new StreamReader(fs, encoding);
             string s;
             List<string> l = new List<string>();
-            while ((s=sr.ReadLine()) != null)
+            while ((s = sr.ReadLine()) != null)
             {
 
                 l.Add(s);
@@ -66,7 +66,50 @@ namespace DLL
             fs.Flush();
             fs.Close();
         }
+        public static void Write(string Str, string FilePath, bool Override , System.Text.Encoding Encoding)
+        {
+            if (!Override)
+            {
+                int i = 1;
+                string DirName = System.IO.Path.GetDirectoryName(FilePath);
+                string FileName = System.IO.Path.GetFileNameWithoutExtension(FilePath);
+                string Extension = System.IO.Path.GetExtension(FilePath);
 
+                while (System.IO.File.Exists(FilePath))
+                {
+
+
+                    FilePath = DirName + "\\" + FileName + "(" + i.ToString() + ")" + Extension;
+                    i++;
+                }
+
+
+            }
+
+
+
+            
+
+
+            StreamWriter sw = new StreamWriter(FilePath,false ,Encoding);
+            sw.WriteLine(Str);
+            sw.Close();
+
+
+            //FileStream fs = new FileStream(FilePath, FileMode.Create);
+            ////获得字节数组
+            //byte[] data = System.Text.Encoding.Default.GetBytes(Str);
+            //data = Encoding.Convert(System.Text.Encoding.Default, Encoding, data);
+
+            ////开始写入
+            //fs.Write(data, 0, data.Length);
+            ////清空缓冲区、关闭流
+            //fs.Flush();
+
+
+
+            //fs.Close();
+        }
 
     }
 }
---
+++ ClassLibrary1/WebGet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace DLL
+{
+    public class WebGet
+    {
+        //ContentType = gb2312  UTF-8
+        public static string HttpGet(string Url, string postDataStr ,string Proxy,string ContentType)
+        {
+            Thread.Sleep(1000);
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+                int aa = request.ServicePoint.ConnectionLimit;
+                request.Timeout = 2000;
+
+
+                if (Proxy != "")
+                {
+                    //request.Proxy = new WebProxy("127.0.0.1:1098");
+                    request.Proxy = new WebProxy(Proxy);
+                }
+
+
+                //request.KeepAlive = false;
+                request.Method = "GET";
+
+                request.ContentType = "text/html;charset=" + ContentType;
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(ContentType));
+
+                string retString = myStreamReader.ReadToEnd();
+
+                myStreamReader.Close();
+                myResponseStream.Close();
+
+                return retString;
+            }
+            catch (Exception)
+            {
+
+                return "";
+            }
+
+        }
+
+
+
+    }
+}
